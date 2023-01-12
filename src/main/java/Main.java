4301c3cdcc9fd4fa48752a6307c8311d7a9416ce
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-12 22:18:09 +0200

    LanguageTask

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
+import com.github.pemistahl.lingua.api.Language;
 import com.github.pemistahl.lingua.api.LanguageDetector;
 
 public class Main {
-    public static <Lingua> void main(String[] args) {
-        Lingua lingua = new Lingua();
+    public static void main(String[] args) {
+       // Lingua lingua = new Lingua();
+       // LanguageDetector languageDetector = new LanguageDetector();
 
         for (int i = 0; i <10; i++) {
-            System.out.println(lingua.l);
+            System.out.println();
 
         }
     }
