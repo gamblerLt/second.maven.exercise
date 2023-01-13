2fd4d310b25b0d6eb484760e46d70283fa06aabe
gamblerLt <rimas.spakauskas@outlook.com>
2023-01-13 11:48:07 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,49 @@
 import com.github.pemistahl.lingua.api.Language;
 import com.github.pemistahl.lingua.api.LanguageDetector;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-       // Lingua lingua = new Lingua();
-       // LanguageDetector languageDetector = new LanguageDetector();
+        // Lingua lingua = new Lingua();
+        List<String> words = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i <10; i++) {
-            System.out.println();
+        Main main = new Main();
+        //patikrinu, ar nesiduplikuoja
+        boolean isWordAlreadyInArray;
 
+        do {
+
+            System.out.println("Iveskite 10 žodžių");
+            String word = sc.nextLine();
+            isWordAlreadyInArray = word.equals("Toks žodis jau yra");
+
+            if (!isWordAlreadyInArray) {
+                main.addWord(words, word);
+                System.out.println(words);
+            }
+
+        } while (!isWordAlreadyInArray);
+
+    }
+
+    private void addWord(List<String> words, String word) {
+        LanguageDetector languageDetector = new LanguageDetector( LanguageDetector(List<>));
+        languageDetector.detectLanguageOf(List<String>words);
+
+        if(words.size()==10) {
+
+            return;
         }
+
+        if(words.contains(word)) {
+            System.out.println("Toks žodis yra");
+            return;
+        }
+    words.add(word);
+
     }
 }
-/*public static void main(String[] args) {
-        Faker faker = new Faker();
-
-        for(int i = 0; i < 100; i++) {
-            System.out.println(faker.address().streetAddress());
-        }*/
